8ad4009859fdd24b380ff85055d6529afcc14c78
Madhan Mohan Reddy Peram <madhan.mohan.reddy.peram@ni.com>
2024-05-16 14:43:04 +0530

    course project http requests

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -25,6 +25,11 @@ export class RecipeService {
     ),
   ];
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
   getRecipes(): Recipe[] {
     return this.recipes.slice();
   }
